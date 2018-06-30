b8ebd38da3764bc9128a6aa979c3c1aacae92e9e
Nora Granahan <nora@punkave.com>
2018-06-30 13:39:23 -0400

    Renders images to the homepage

--- client/src/components/Photo/Photo.js
+++ client/src/components/Photo/Photo.js
@@ -1,221 +1,51 @@
 
 import React, { Component } from 'react';
+import API from '../../utils/API';
 import { Link } from 'react-router-dom';
-import { Grid, Row, Col, Image, Button,Thumbnail } from 'react-bootstrap';
+import { Grid, Row, Col, Image, Button, Thumbnail } from 'react-bootstrap';
 import './Photo.css';
-
+import { ListGroup } from 'react-bootstrap';
 
 export default class Photo extends Component {
 
-    render() {
-      return ( 
-<Grid> 
-  <Row>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-  </Row>
+  state = {
+    artwork: []
+  };
 
-  <Row>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-  </Row>
+  componentDidMount() {
+    this.loadArt();
+  }
+
+  loadArt = () => {
+    console.log("loading artwork");
+    API.getArt()
+      .then(res => {
+        this.setState({ artwork: res.data })
+        console.log(this.state.artwork[0].title)
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.artwork.length ? (
+          <Grid className="photo-container">
+
+            {this.state.artwork.map(artwork => (
+              
+
+              <Thumbnail className="photo-thumbnail" src={artwork.imageurl} alt="242x200"><h2>{artwork.title}</h2></Thumbnail>
+            ))}
+
+          </Grid>
+        ) : (
+            <h3>No Results to Display</h3>
+          )}
+      </div>
+      );
+
+  }
+}
 
 
-  <Row>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-  </Row>
-
-
-  <Row>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-  </Row>
-
-
-
-  <Row>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-  </Row>
-
-
-  <Row>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-    <Col xs={6} md={4}>
-      <Thumbnail src="/thumbnaildiv.png" alt="242x200">
-        <h3>Thumbnail label</h3>
-        <p>Description</p>
-        <p>
-          <Button bsStyle="primary">Button</Button>&nbsp;
-          <Button bsStyle="default">Button</Button>
-        </p>
-      </Thumbnail>
-    </Col>
-  </Row>
-
-
- </Grid>);
-
-}}
-
- 
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import API from '../utils/API';
+// import API from '../utils/API';
 import Navbar from "../components/Navbar";
 import SearchBar from "../components/SearchBar";
 import Photo from "../components/Photo";
@@ -8,34 +8,16 @@ import { Link } from 'react-router-dom';
 import 'react-bootstrap';
 import './Homepage.css';
 
+
 class Homepage extends Component {
-    state = {
-        artwork: []
-      };
-    
-      componentDidMount() {
-        this.loadArt();
-      }
-    
-      loadArt = () => {
-          console.log("loading artwork");
-        API.getArt()
-          .then(res => {
-            this.setState({ artwork: res.data })
-            console.log(this.state.artwork[0].title)
-          })
-          .catch(err => console.log(err));
-      };
- 
+
     render() {
         // return (<div></div>)
        return (
        <div>
                 <Navbar />
                 <SearchBar />
-
                 <Photo />
-
                 <Footer />
 
         </div>
