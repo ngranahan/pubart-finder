aae6a69d015a11b6680026a97c2e0fdfc5ed582c
Nora Granahan <nora@punkave.com>
2018-07-17 20:03:24 -0400

    Fixes login and register pages

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -74,7 +74,7 @@ class Login extends Component {
               
 
                     <div class="logBody">
-                    <p className="lead" className= "text-center"> <h4> Please enter your credentials below. </h4></p>
+                    <p className="lead" className= "text-center">Please enter your credentials below.</p>
                     <br />
                    
                     
@@ -97,14 +97,12 @@ class Login extends Component {
                 </div>
 
 
-           <Row className="show-grid">
-                <Col  xs={6} md={2} xsOffset={4}>
+           <div className="show-grid text-center">
+                
                     <button className="btn button1" type='submit' onClick={this.handleFormSubmit}>Submit</button>&nbsp;
-                </Col>
-                <Col  xs={6} md={4} xsOffset={2}>
                     <a className="btn button2" href='/'>Cancel</a> 
-               </Col>
-         </Row>      
+               
+         </div>      
                     </form>
                     </div>
             </div>
